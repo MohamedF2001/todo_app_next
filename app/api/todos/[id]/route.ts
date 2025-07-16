3db48ad6988192a8bb25d16d8935b62ce4566f8b
frdmo <frdmoussiliou@gmail.com>
2025-07-16 11:13:49 +0100

    last modif

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -22,7 +22,7 @@ export async function DELETE(_: Request, { params }: { params: { id: string } })
 }
  */
 
-import { connectDB } from '@/lib/db';
+/* import { connectDB } from '@/lib/db';
 import Todo from '@/models/todo';
 import { NextResponse } from 'next/server';
 
@@ -50,6 +50,42 @@ export async function PUT(request: Request, context: Params) {
 
 // DELETE /api/todos/:id
 export async function DELETE(_: Request, context: Params) {
+    await connectDB();
+    await Todo.findByIdAndDelete(context.params.id);
+    return NextResponse.json({ success: true });
+}
+ */
+
+import { connectDB } from '@/lib/db';
+import Todo from '@/models/todo';
+import { NextResponse } from 'next/server';
+
+// GET /api/todos/:id
+export async function GET(
+    _: Request,
+    context: { params: { id: string } }
+) {
+    await connectDB();
+    const todo = await Todo.findById(context.params.id);
+    return NextResponse.json(todo);
+}
+
+// PUT /api/todos/:id
+export async function PUT(
+    request: Request,
+    context: { params: { id: string } }
+) {
+    await connectDB();
+    const data = await request.json();
+    const updated = await Todo.findByIdAndUpdate(context.params.id, data, { new: true });
+    return NextResponse.json(updated);
+}
+
+// DELETE /api/todos/:id
+export async function DELETE(
+    _: Request,
+    context: { params: { id: string } }
+) {
     await connectDB();
     await Todo.findByIdAndDelete(context.params.id);
     return NextResponse.json({ success: true });
